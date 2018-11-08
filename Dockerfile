2b02a10a9be173b78ea6ed57117a40dc0ed17210
Bryan Stitt <bryan@stitthappens.com>
2018-11-08 09:44:49 -0800

    i really should commit more
    Use yarn
    Update to the latest 0x libs
    Also setup expiration watcher
    add endpoint for stopping watching an order
    probably other stuff, too

--- Dockerfile
+++ Dockerfile
@@ -1,26 +1,17 @@
-FROM node:8
+FROM node:10-stretch
 
-# Create app directory
-WORKDIR /usr/src/app
+RUN mkdir -p /usr/src/myapp
+WORKDIR /usr/src/myapp
 
-# Install app dependencies
-# A wildcard is used to ensure both package.json AND package-lock.json are copied
-# where available (npm@5+)
-COPY package*.json ./
-COPY .env.example .env
+# install dependencies
+COPY package.json .
+COPY yarn.lock .
+RUN yarn install
 
-RUN npm install
-# If you are building your code for production
-# RUN npm install --only=production
-
-# Bundle app source
+# install the app
 COPY . .
+RUN yarn install
 
-EXPOSE 3000
-CMD [ "npm", "start" ]
-
-# Environment Variables
-ENV PROVIDER 'http://localhost:8545'
-ENV NETWORK_ID 42
-ENV PORT 3000
-ENV RELAYER 'http://localhost:8080/api/0x/v0/order/'
+# run the app
+ENTRYPOINT [ "yarn" ]
+CMD [ "run", "start" ]
--- index.js
+++ index.js
@@ -1,70 +1,104 @@
-const OrderWatcher = require('@0xproject/order-watcher').OrderWatcher;
-const Web3 = require('web3');
-const bodyParser = require('body-parser');
-const express = require('express');
-const BigNumber = require('@0xproject/utils').BigNumber;
-const request = require('request');
-require('dotenv').config();
+const BigNumber = require("@0x/utils").BigNumber;
+const bodyParser = require("body-parser");
+const express = require("express");
+const ExpirationWatcher = require("@0x/order-watcher").ExpirationWatcher;
+const OrderWatcher = require("@0x/order-watcher").OrderWatcher;
+const request = require("request");
+const Web3 = require("web3");
 
-const provider = new Web3.providers.HttpProvider(process.env.PROVIDER);
-const orderWatcher = new OrderWatcher(provider, +process.env.NETWORK_ID);
+// TODO: https://github.com/0xProject/0x-monorepo/pull/1227
+const provider = new Web3.providers.HttpProvider(process.env.ETHEREUM_HTTP_PROVIDER);
+// const provider = new Web3.providers.WebsocketProvider(process.env.ETHEREUM_WS_PROVIDER);
 
+const orderWatcher = new OrderWatcher(provider, +process.env.ETHEREUM_NETWORK_ID);
+const expirationWatcher = new ExpirationWatcher(provider, +process.env.ETHEREUM_NETWORK_ID);
+
+// TODO: expirationWatcher.subscribe(...); should be very similar
 orderWatcher.subscribe((error, orderState) => {
-    console.log(orderState);
-    if (orderState.isValid) {
-        for (let k in orderState.orderRelevantState)
-            orderState.orderRelevantState[k] = orderState.orderRelevantState[k].toString();
-    } else {
-        orderWatcher.removeOrder(orderState.orderHash);
+  console.log(orderState);
+  if (orderState.isValid) {
+    // TODO: why is this toString necessary?
+    for (let k in orderState.orderRelevantState) {
+      orderState.orderRelevantState[k] = orderState.orderRelevantState[k].toString();
     }
-    request.post(
-        process.env.RELAYER + orderState.orderHash,
-        {json: true, body: orderState},
-        (err, res, body) => {
-            if (err) { return console.log(err); }
-            console.log(body);
-        });
+  } else {
+    // expirationWatcher.removeOrder(orderState.orderHash);
+    orderWatcher.removeOrder(orderState.orderHash);
+  }
+
+  // TODO: use websocket instead of http
+  request.post(
+    process.env.RELAYER_HTTP + orderState.orderHash,
+    { json: true, body: orderState },
+    (err, res, body) => {
+      if (err) {
+        return console.log(err);
+      }
+      console.log(body);
+    }
+  );
 });
 
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/v0/order', (req, res) => {
-    console.log('HTTP: POST order');
-    const order = req.body;
+app.post("/v0/order", (req, res) => {
+  // TODO: receive websocket request instead of POST
+  console.log("HTTP: POST order");
+  const order = req.body;
 
-    try {
-        orderWatcher.addOrder(convertToBigNumber(order));
-        res.status(201).send({});
-        console.log('Watching order ' + order.orderHash);
-    } catch (e) {
-        res.status(400).send(e.message)
-    }
+  try {
+    order = convertToBigNumber(order);
 
+    orderWatcher.addOrder(order);
+    expirationWatcher.addOrder(order);
+
+    // TODO: what should we return?
+    res.status(201).send(order.orderHash);
+    console.log("Watching order " + order.orderHash);
+  } catch (e) {
+    // TODO: what status code? 400 or 500?
+    res.status(400).send(e.message);
+  }
 });
 
-app.listen(process.env.PORT, () => {
-    console.log('Order watcher listening on port' + process.env.PORT);
-    console.log('Provider: ' + process.env.PROVIDER);
-    console.log('Relayer: ' + process.env.RELAYER);
-    console.log('Network Id: ' + process.env.NETWORK_ID)
+app.delete("/v0/order", (req, res) => {
+  // TODO: receive websocket request instead of POST
+  console.log("HTTP: DELETE order");
+  const orderHash = req.body;
+
+  try {
+    orderWatcher.removeOrder(orderHash);
+    expirationWatcher.removeOrder(orderHash);
+
+    // TODO: what should we return?
+    res.status(200).send(orderHash);
+    console.log("Not watching order " + orderHash);
+  } catch (e) {
+    // TODO: what status code? 400 or 500?
+    res.status(400).send(e.message);
+  }
 });
 
-const keys = [
-    'salt',
-    'makerFee',
-    'takerFee',
-    'makerTokenAmount',
-    'takerTokenAmount',
-    'expirationUnixTimestampSec',
+app.listen(process.env.WATCHER_PORT, () => {
+  console.log("Order watcher listening on port " + process.env.WATCHER_PORT);
+  console.log("Provider: " + process.env.ETHEREUM_WS_PROVIDER);
+  console.log("Relayer HTTP: " + process.env.RELAYER_HTTP);
+  console.log("ETH Network Id: " + process.env.ETHEREUM_NETWORK_ID);
+});
+
+const big_number_keys = [
+  "salt",
+  "makerFee",
+  "takerFee",
+  "makerTokenAmount",
+  "takerTokenAmount",
+  "expirationUnixTimestampSec"
 ];
-
 function convertToBigNumber(order) {
+  big_number_keys.forEach(function(k) {
+    if (typeof order[k] != "undefined") order[k] = new BigNumber(order[k]);
+  });
 
-    keys.forEach(function (k) {
-        if (typeof order[k] != 'undefined')
-            order[k] = new BigNumber(order[k]);
-    });
-
-    return order;
+  return order;
 }
